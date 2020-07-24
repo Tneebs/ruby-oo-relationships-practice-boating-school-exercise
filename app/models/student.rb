92ca046f54f729febc8090b294fae213705e898e
Tneebs <tedneben@gmail.com>
2020-07-24 11:46:37 -0500

    Update 1

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,23 @@
 class Student
+    attr_accessor :first_name, :student
+
+    @@all = []
+
+    def initialize(first_name)
+        @first_name = first_name
+        @@all << self
+    end
+
+    def add_boating_test(test_name, test_status, student, instructor)
+        BoatingTest.new(test_name, test_status, student, instructor)
+    end
+
+    def self.find_student(first_name)
+        Student.all.find_by {|student| student.first_name == first_name}
+    end
+    
+    def self.all 
+        @@all
+    end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -8,6 +8,14 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+student1 = Student.new("Wilmar")
+student2 = Student.new("John")
+student3 = Student.new("Ted")
+
+instructor1 = Instructor.new("Angelo")
+instructor1 = Instructor.new("Shivang")
+
+bt1 = BoatingTest.new("test1", "Pass", student1, instructor1)
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
 
